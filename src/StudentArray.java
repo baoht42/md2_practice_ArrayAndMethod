fe4c18663c8b86296d241a46d033ec819cf39db4
baoht42 <snorlax4293@gmail.com>
2023-12-12 14:17:03 +0700

    nop bai tim kiem phan tu trong mang

---
+++ src/StudentArray.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+public class StudentArray {
+    public static void main(String[] args) {
+        String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name to search: ");
+        String inputName = sc.nextLine();
+        boolean isCheck = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(inputName)){
+                System.out.println("Founded: "+inputName +" in the array");
+                isCheck = true;
+                break;
+            }
+
+            if (!isCheck){
+                System.out.println("Not found "+inputName + " in the array");
+            }
+        }
+    }
+
+}
